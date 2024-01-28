465e2f8f19d0fe6a235ab96f66c4e8a4c3c333a5
Kulpot <kulpotwahu@gmail.com>
2024-01-28 14:03:05 +0800

    ...

--- C++ reinterpret_cast/CppScratchPad.cpp
+++ C++ reinterpret_cast/CppScratchPad.cpp
@@ -5,7 +5,9 @@ using std::endl;
 // --------------------------- C++ reinterpret_cast -----------------------------------------------------
 //ref link:https://www.youtube.com/watch?v=wcKXGvi1JRk&list=PLRwVmtr-pp05motkiTlDIuU5ZfoRr-kWC&index=2&t=7s
 
-struct Cow
+
+
+struct Cow		//struct defaults to public
 {
 	int mooCount;
 	int legCount;
@@ -15,7 +17,7 @@ struct Cow
 
 int main()
 {
-	Cow cow;
+	Cow cow;		// cow contains 2x int, char, bool, packing
 	cow.mooCount = 8; cow.legCount = 5;
 	cow.gender = 'f'; cow.isButchered = true;
 }
