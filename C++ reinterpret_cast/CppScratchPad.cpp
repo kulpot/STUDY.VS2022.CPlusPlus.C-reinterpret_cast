7c2d0b5f4c07998629e83f0336a47020d0e62e40
Kulpot <kulpotwahu@gmail.com>
2024-01-28 14:46:54 +0800

    ...

--- C++ reinterpret_cast/CppScratchPad.cpp
+++ C++ reinterpret_cast/CppScratchPad.cpp
@@ -5,7 +5,9 @@ using std::endl;
 // --------------------------- C++ reinterpret_cast -----------------------------------------------------
 //ref link:https://www.youtube.com/watch?v=wcKXGvi1JRk&list=PLRwVmtr-pp05motkiTlDIuU5ZfoRr-kWC&index=2&t=7s
 
+//reinterpret cast allows you to cast a pointer type into another type
 //reinterpret casting - does not change the address while changing the value type of a RAM in compile time
+//one thing to point out is reinterpret cast should be used keeping in mind the packing done by the compiler and if you don't want to check the packing, then just use pragma pack 1(compiler optimization won't be applied and there would be no packing)
 
 struct Cow		//struct defaults to public
 {
@@ -17,7 +19,7 @@ struct Cow		//struct defaults to public
 
 int main()
 {
-	Cow cow;		// cow contains 2x int, char, bool, packing
+	Cow cow;		// cow contains 2x int, char, bool, packing(compiler)
 	cow.mooCount = 8; cow.legCount = 5;
 	cow.gender = 'f'; cow.isButchered = true;
 	Cow* cowPtr = &cow;		//cow pointer - address
@@ -39,7 +41,7 @@ int main()
 	cout << *reinterpret_cast<char*>(intPtr) << endl;	// points to gender(char value) and the value char is not save to char point value
 	char* charPtr = reinterpret_cast<char*>(intPtr);
 	//cout << *charPtr << endl;	// stored in charPtr value
-	charPtr++;
+	charPtr++;			// *charPtr pointer move to 4 bytes
 	cout << *charPtr << endl;	// charPtr points to isButchered(charr value)
 	cout << *reinterpret_cast<bool*>(charPtr) << endl;	// charPtr points to isButchered(bool value)
 	cout << true << endl; // prove bool result of isButchered bool value
