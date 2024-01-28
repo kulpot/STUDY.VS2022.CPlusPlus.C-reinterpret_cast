9836cd176f6b16062337f7cf102e2dc03ecba439
Kulpot <kulpotwahu@gmail.com>
2024-01-28 14:00:07 +0800

    ...

--- C++ reinterpret_cast/CppScratchPad.cpp
+++ C++ reinterpret_cast/CppScratchPad.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using std::cout;
-using std::cout;
+using std::endl;
 
 // --------------------------- C++ reinterpret_cast -----------------------------------------------------
 //ref link:https://www.youtube.com/watch?v=wcKXGvi1JRk&list=PLRwVmtr-pp05motkiTlDIuU5ZfoRr-kWC&index=2&t=7s
